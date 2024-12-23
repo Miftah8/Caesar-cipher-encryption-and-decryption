eadfaed663fe44ff008f15f36ec42d0c7979c80b
Kyy0x0 <146458421+Miftah8@users.noreply.github.com>
2024-12-23 14:35:09 +0700

    Add files via upload

---
+++ welcome.py
@@ -0,0 +1,20 @@
+up = r"""
+================================================================================
+Welcome to Caesar Cipher Algorithm!
+
+Caesar Cipher is a cryptographic technique that uses substitution techniques to 
+encrypt and decrypt text. The Caesar Cipher is one of the oldest and simplest
+cryptographic techniques.
+ 
+   _____                              _____ _       _                
+  / ____|                            / ____(_)     | |               
+ | |     __ _  ___  ___  __ _ _ __  | |     _ _ __ | |__   ___ _ __  
+ | |    / _` |/ _ \/ __|/ _` | '__| | |    | | '_ \| '_ \ / _ \ '__| 
+ | |___| (_| |  __/\__ \ (_| | |    | |____| | |_) | | | |  __/ |    
+  \_____\__,_|\___||___/\__,_|_|     \_____|_| .__/|_| |_|\___|_|    
+                                             | |                     
+                                             |_|                     
+                                             
+Code by:
+Miftah Rizky Alamsyah
+================================================================================"""
